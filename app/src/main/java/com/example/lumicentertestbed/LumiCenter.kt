4b3512894d5138ab26c92a7d3edc9d7329791bcc
Keith Matthews <keith@keithdmatthews.com>
2020-04-25 13:30:37 -0700

    Tons of debug Log.d commands.  To be fixed.

--- app/src/main/java/com/example/lumicentertestbed/LumiCenter.kt
+++ app/src/main/java/com/example/lumicentertestbed/LumiCenter.kt
@@ -1,7 +1,9 @@
 package com.example.lumicentertestbed
 
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
+import android.util.Log
 import android.widget.ImageView
 import java.nio.ByteBuffer
 
@@ -9,6 +11,10 @@ class LumiCenter(val imageView: ImageView) {
     private val width: Int = imageView.width
     private val height: Int = imageView.height
     val bitmap: Bitmap = convertImageViewToBitmap(imageView)
+
+    companion object {
+        private const val TAG = "LumiCenter"
+    }
 
 //    fun convertByteToInt(byte: Byte): Int {
 //        return byte.toInt() and 0xFF
@@ -19,21 +25,41 @@ class LumiCenter(val imageView: ImageView) {
 //    }
 
     private fun convertImageViewToBitmap(imageView: ImageView): Bitmap {
+
+//        if(imageView == null)
+//            Log.d(TAG, "imageView was null!!!")
+//        if(imageView.drawable == null)
+//            Log.d(TAG, "imageView.drawable was null!!!")
+//
+//        Log.d(TAG, "Is this class populated? ${imageView.drawable}")
+
         // convert to Bitmap
         // https://stackoverflow.com/questions/4715044/android-how-to-convert-whole-imageview-to-bitmap
         return (imageView.drawable as BitmapDrawable).bitmap
     }
 
+    fun extractLumi(color: Int): Int {
+        // https://developer.android.com/reference/android/graphics/Color
+        val albedo: Int = (color shr 24) and 0xFF
+        val red: Int = (color shr 16) and 0xFF
+        val green: Int = (color shr 8)  and 0xFF
+        val blue: Int = color and 0xFF
+//        Log.d(TAG, "color values for that pixel are A: $A, R: $R, G: $G and B: $B")
+
+        return red
+    }
+
     fun sumOverImage(mult: (Int, Int) -> Int): Double {
+        Log.d(TAG,"Random point on bitmap had R = ${extractLumi(bitmap.getPixel(100, 100))}")
+
         var acc = 0.0
         for (x in 0 until width - 1) {
             for (y in 0 until height - 1) {
-                acc += bitmap.getPixel(x, y) * mult(x, y)
+//                acc += extractLumi(bitmap.getPixel(x, y)) * mult(x,y)
+                acc += extractLumi(bitmap.getPixel(x, y))
             }
         }
         return acc
     }
-
-
 }
 
--- app/src/main/java/com/example/lumicentertestbed/MainActivity.kt
+++ app/src/main/java/com/example/lumicentertestbed/MainActivity.kt
@@ -7,33 +7,97 @@ import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 import java.io.InputStream
 import kotlin.math.roundToInt
+import kotlin.reflect.typeOf
 
 //import androidx.camera.view.PreviewView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var constraintLayout: ConstraintLayout
 
+    companion object {
+        private const val TAG = "lumiCenterTestBed"
+    }
 
+    // following example code in:
+    // https://developer.android.com/guide/topics/graphics/drawables
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        val imageDisplay = findViewById<ImageView>(R.id.image_display)
+        // Instantiate an ImageView and define its properties
+        val imageView = ImageView(this).apply {
+            setImageResource(R.drawable.lowerleftblob01)
+            contentDescription = resources.getString(R.string.lowerleftblob01_desc)
 
-        val imageView: ImageView = readImageFromFile("lowerleftblob01.jpg")
+            // set the ImageView bounds to match the Drawable's dimensions
+            adjustViewBounds = true
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+        }
 
+//        val analysisResults = TextView(this).apply {
+//            text = "to be replaced",
+//            id = findViewById<TextView>(R.id.analysisResults),
+//            layoutParams = ViewGroup.LayoutParams(
+//                this,
+//                textSize = "30sp",
+//                ViewGroup.LayoutParams.WRAP_CONTENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT
+//            )
+//        }
+
+        // Create a ConstraintLayout in which to add the ImageView
+        constraintLayout = ConstraintLayout(this).apply {
+
+            // Add the ImageView to the layout
+            addView(imageView)
+            // Add the TextView to the layout
+//            addView(analysisResults)
+        }
+
+        // Set the layout as the content view.
+        setContentView(constraintLayout)
+
+
+        // ----------------------------------------
+
+//        val imageView: ImageView = readImageFromFile("lowerleftblob01.jpg")
+        Log.d(TAG, "just read from file: $imageView")
+
+//        val imageDisplay = findViewById<ImageView>(R.id.image_display)
+//        imageDisplay.setImageResource(imageDisplay.id)
+//        Log.d(TAG, "assigned image to display $imageDisplay")
+
+        // crashes on this constructor
         val lumiCenter = LumiCenter(imageView)
-        imageDisplay.setImageBitmap(lumiCenter.bitmap)
+//        imageDisplay.setImageBitmap(lumiCenter.bitmap)
 
-        val sumOnVals = lumiCenter.sumOverImage() {_, _ -> 1}
-        val xEst: Int = (lumiCenter.sumOverImage() {x: Int, _ -> x}/sumOnVals).roundToInt()
-        val yEst: Int = (lumiCenter.sumOverImage() {_, y: Int -> y}/sumOnVals).roundToInt()
+        var sumOnVals = lumiCenter.sumOverImage() { _, _ -> 1 }
 
-        analysisResults.text = "$xEst, $yEst"
+        Log.d(TAG, "sumOnVals = $sumOnVals")
+        val xEst: Double = (lumiCenter.sumOverImage { x: Int, _ -> x})
+        Log.d(TAG, "x-center: $xEst")
+        val yEst: Double = (lumiCenter.sumOverImage { _, y: Int -> y})
+        Log.d(TAG, "y-center: $yEst")
+
+
+//        val analysisResults = findViewById<TextView>(R.id.analysisResults)
+
+        Log.d(TAG, "analysisResults is null? $analysisResults")
+
+        // Attempt to invoke virtual method 'java.lang.Class java.lang.Object.getClass()' on a null object reference
+        Log.d(TAG, "type of analysisResults: ${analysisResults::class.simpleName}")
+        analysisResults.text = "${xEst.toInt()}, ${yEst.toInt()}"
+
     }
 
 
